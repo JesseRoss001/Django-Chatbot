066555fc8a7dcfe01962652bff4405bf5eaec615
Jesse Lee Ross <79084912+JesseRoss001@users.noreply.github.com>
2024-06-06 20:56:40 +0000

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Django-Chatbot
-Django Chatbot 
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True' 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://jesseross001.github.io/Django-Chatbot/"]
 
 
 # Application definition
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # settings.py
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # Ensure CSRF Middleware if your form requires it
